1d1cc31c917ee0c9a2156577bcaed9cebbcc1a66
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-12-21 15:39:58 -0600

    blog-app-added

--- 22-django/ProyectoDjango/ProyectoDjango/settings.py
+++ 22-django/ProyectoDjango/ProyectoDjango/settings.py
@@ -125,3 +125,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CKEDITOR
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['Table', 'Image'],
+            ['RemoveFormat', 'Source'],
+            ['Image', 'Table']
+        ]
+    }
+}
