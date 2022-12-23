075f9d424bc255c56bf59cf02ed9c1190722ef17
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-12-23 00:37:45 -0600

    conf-admin-dashbord-done

--- 22-django/ProyectoDjango/pages/admin.py
+++ 22-django/ProyectoDjango/pages/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Page
 
+# Configuración extra
+class PageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+    search_fields = ('title', 'content') # Buscador
+    list_filter = ('visible', ) # Filtrar por campos del modelo
+    list_display = ('title', 'visible', 'created_at') # Mostrar solo algunas colums del modelo
+    ordering = ('-created_at',) # Criterio de ordenación
+
 # Register your models here.
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 
 # Configuración del panel
 title = "Proyecto con Django"
