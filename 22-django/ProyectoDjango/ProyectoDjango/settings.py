43de4828e88ff6323e2e3b5a8131e50a958c8994
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-12-24 23:52:48 -0600

    comment-sqlite-db

--- 22-django/ProyectoDjango/ProyectoDjango/settings.py
+++ 22-django/ProyectoDjango/ProyectoDjango/settings.py
@@ -78,13 +78,14 @@ WSGI_APPLICATION = 'ProyectoDjango.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+"""
 
 
 # Password validation
