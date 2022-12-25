d92ac690f38f2570378b5cc5c79a91fea874eded
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-12-25 16:12:11 -0600

    mysql-connect-and-api-rest-full-views

--- 22-django/ProyectoDjango/blog/urls.py
+++ 22-django/ProyectoDjango/blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('articulos/', views.list, name="list_articles"),
     path('categoria/<int:category_id>/', views.category, name="category"),
     path('articulo/<int:article_id>/', views.article, name="article"),
+    path('test-json/', views.test, name="test_json")
 ]
--- 22-django/ProyectoDjango/blog/views.py
+++ 22-django/ProyectoDjango/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from blog.models import Category, Article
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
 
 # Create your views here.
 @login_required(login_url='login')
@@ -38,3 +40,32 @@ def article(request, article_id):
     return render(request, 'articles/detail.html', {
         'article': article
     })
+
+@csrf_exempt
+def test(request):
+    if request.method == 'POST':
+        response = {
+            'name': 'milton',
+            'surname': 'ponce'
+        }
+    elif request.method == 'PUT':
+        response = {
+            'method': 'PUT',
+            'surname': 'ponce'
+        }
+    elif request.method == 'DELETE':
+        response = {
+            'method': 'DELETE',
+            'surname': 'ponce'
+        }
+    elif request.method == 'GET':
+        response = {
+            'method': 'GET',
+            'name': 'milton'
+        }
+    else:
+        response = {
+            'error': 'NO ES NINGUNA HTTP REQUEST'
+        }
+
+    return JsonResponse(response)
